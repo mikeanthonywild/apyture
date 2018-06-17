132db89a02ec6166d19e63b9068e7ea90eab6e58
Mike Wild <mike@mikeanthonywild.com>
2018-06-17 18:51:52 +0100

    Commit changes fromo Chromebook

--- examples/raspi/raspi/raspi.py
+++ examples/raspi/raspi/raspi.py
@@ -1,4 +1,4 @@
-from apyture import Device, StateMachineMixin, LogParser, Uart
+from apyture import Device, StateMachineMixin, LogParser, Uart, state
 
 
 class RaspiParser(LogParser):
@@ -6,28 +6,30 @@ class RaspiParser(LogParser):
         self.
 
 
-class RasberryPi(StateMachineMixin, Device):
+class RaspberryPi(StateMachineMixin, Device):
     DEFAULT_TIMEOUT = 30 # seconds
     UART_BAUD_RATE = 115700
 
     def __init__(self, mac_address, serial_port):
         super().__init__()
-    
-        self._uart = Uart(device=serial_port, baud_rate=self.UART_BAUD_RATE) 
-        
-        # Setup states
-        self.add_state('IDLE', self._do_idle)
 
-        # Callbacks
-        self.register_callback('UBOOT_STARTED_KERNEL', self._kernel_started_cb)
-        self.register_callback('BOOTED', self._booted_cb)
-
+        self._uart = Uart(device=serial_port, baud_rate=self.UART_BAUD_RATE)
         # Flags
         self._kernel_started = Flag()
         self._booted = Flag()
 
+
+    # State machine states
+    @state('IDLE')
     def _do_idle(self):
         self.cpu.start()
+        return 'WAIT_FOR_UBOOT'
+
+
+    @state('WAIT_FOR_UBOOT')
+    def _do_wait_for_uboot(self):
+        
+
 
     def _execute_serial_cmd(self, cmd):
         self._uart.write(cmd)
@@ -35,26 +37,30 @@ class RasberryPi(StateMachineMixin, Device):
             out = self._uart.read()
         except SerialTimeout:
             out = ''
- 
+
         return out
 
     def kernel_started_cb(self):
         self._kernel_started.set()
 
+
     def booted_cb(self):
         self._booted.set()
 
+
     def reset(self):
         super().reset()
- 
+
         if not self._kernel_started():
             self._execute_serial_cmd('reboot')
         else:
             self.wait_until_booted()
             self._execute_serial_cmd('sudo reboot')
 
+
     def wait_until_booted(self):
         self._booted.wait(timeout=DEFAULT_TIMEOUT)
+
 
     def wait_until_ip_address_assigned(self):
         self._ip_address_assigned.wait(timeout=DEFAULT_TIMEOUT)
@@ -66,12 +72,13 @@ class RasberryPi(StateMachineMixin, Device):
 
 
 class ModelB(RaspberryPi):
+    # Feels a bit awkward. Would a decorator be better?
     memory_size = CharField(default='1024 MB')
 
 
 class PiZero(RaspberryPi):
     memory_size = CharField(default='512 MB')
-    
+
 
 @apyture.testutils.fixture
 def dut():
